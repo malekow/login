8f8b5595d3790fdaa6a37a81a650d82369a47fd7
malekow <abukarabdullahi90@gmail.com>
2023-04-08 03:56:56 -0700

    test

---
+++ index.js
@@ -0,0 +1,59 @@
+
+import Express from 'express';
+import mongoose from 'mongoose'
+import User from './modules/usermodules.js';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const app = Express();
+const port = 3000;
+app.use(Express.json())
+
+app.get('/', async (req, res)=>{
+  const users = await User.find()
+
+  res.json(users)
+})
+
+app.post('/', async(req, res)=>{
+     const { name, email, password } = req.body
+    const user = new User({
+        name, email, password
+    });
+
+    const newdata = await user.save();
+    res.status(201).json(newdata);
+})
+
+app.put('/:id', async(req, res)=>{
+const {id}=req.params
+const user = await User.findByIdAndUpdate(id, req.body)
+
+if (user) {
+    const updateuser = await User.findById(id);
+    res.json(updateuser)
+}
+})
+
+app.delete('/:id', async(req, res)=>{
+const {id}=req.params;
+
+const user = await User.findByIdAndDelete(id);
+res.json({message: 'successfully deleted'})
+
+})
+
+
+
+app.listen(port, ()=> {
+    console.log(`server is running on port ${port}`)
+})
+
+
+mongoose.connect(process.env.MONG_URL)
+.then(()=> {
+    console.log('connected to Database')
+}).catch((e) =>{
+    console.log(e);
+})
